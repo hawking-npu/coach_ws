5ad2984010e4ae6f52c3d4645017a81429eaf8ed
Pedestrain233 <382127999@qq.com>
2019-07-15 18:25:45 +0800

    a

---
+++ build/catkin_generated/setup_cached.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/home/james/github_ws/devel:$CMAKE_PREFIX_PATH"
+export PWD="/home/james/github_ws/build"
+export PYTHONPATH="/home/james/coach_ws-master/devel/lib/python2.7/dist-packages:/home/james/nubot_ws/devel/lib/python2.7/dist-packages:/opt/ros/kinetic/lib/python2.7/dist-packages"
+export ROSLISP_PACKAGE_DIRECTORIES="/home/james/github_ws/devel/share/common-lisp:$ROSLISP_PACKAGE_DIRECTORIES"
+export ROS_PACKAGE_PATH="/home/james/github_ws/src:$ROS_PACKAGE_PATH"
